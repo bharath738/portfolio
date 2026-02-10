b2f21d13bfdf7254c5111714cc3628c7243656f1
bharath738 <136260581+bharath738@users.noreply.github.com>
2026-02-10 18:01:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const sections = document.querySelectorAll("section");
 
 const observer = new IntersectionObserver(
@@ -13,19 +12,3 @@ const observer = new IntersectionObserver(
 );
 
 sections.forEach(section => observer.observe(section));
-=======
-const sections = document.querySelectorAll("section");
-
-const observer = new IntersectionObserver(
-  entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      }
-    });
-  },
-  { threshold: 0.15 }
-);
-
-sections.forEach(section => observer.observe(section));
->>>>>>> d77d7164984adf1f22cd08875327c19be9721c15
